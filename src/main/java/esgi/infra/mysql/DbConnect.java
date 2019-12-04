477d292700e319a270d69b6ab90f5ddb30340d51
Massicilia <massicilia.nedir@outlook.com>
2019-12-04 03:13:49 +0100

    deployment db

--- src/main/java/esgi/infra/mysql/DbConnect.java
+++ src/main/java/esgi/infra/mysql/DbConnect.java
@@ -9,11 +9,14 @@ public class DbConnect {
     public static java.sql.Connection connection = null;
 
     public static java.sql.Connection getConnection() {
-        String url = "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_bab9f081f17abbf?reconnect=true"; //jdbc:"+System.getenv( "JDBC_DATABSE_URL");//"jdbc:mysql://localhost:3306/cowork?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        //String url = "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_bab9f081f17abbf?reconnect=true"; //jdbc:"+System.getenv( "DB_RDS");//"jdbc:mysql://localhost:3306/cowork?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String url = System.getenv( "DB_RDS");
         //String url = "jdbc:mysql://localhost:3306/cowork?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-        String username = "b78ab6ae03f754";//System.getenv("JDBC_DATABSE_USERNAME");
+        //String username = "b78ab6ae03f754";//System.getenv("JDBC_DATABSE_USERNAME");
+        String username = System.getenv( "DB_USERNAME_RDS");
         //String username = "root";
-        String password = "358baaea";//System.getenv("JDBC_DATABSE_PASSWORD");
+        //String password = "358baaea";//System.getenv("JDBC_DATABSE_PASSWORD");
+        String password = System.getenv( "DB_PASSWORD_RDS");//System.getenv("JDBC_DATABSE_PASSWORD");
         //String password = "root";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
