e9136146490d0ca9bf774fe6058d640fa3606c88
Massicilia <massicilia.nedir@outlook.com>
2019-12-04 03:22:59 +0100

    deployment db

--- src/main/java/esgi/infra/mysql/DbConnect.java
+++ src/main/java/esgi/infra/mysql/DbConnect.java
@@ -11,9 +11,9 @@ public class DbConnect {
     public static java.sql.Connection getConnection() {
         String url = "jdbc:mysql://us-cdbr-iron-east-05.cleardb.net/heroku_bab9f081f17abbf?reconnect=true"; //jdbc:"+System.getenv( "DB_RDS");//"jdbc:mysql://localhost:3306/cowork?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         //String url = "jdbc:mysql://localhost:3306/cowork?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-        String username = "b78ab6ae03f754";//System.getenv("JDBC_DATABSE_USERNAME");
+        String username = System.getenv("JDBC_DATABSE_USERNAME");
         //String username = "root";
-        String password = "358baaea";//System.getenv("JDBC_DATABSE_PASSWORD");
+        String password = System.getenv("JDBC_DATABSE_PASSWORD");
         //String password = "root";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
