2cbf37e46be8df7ab0e2865b7729703dc73c4137
Massicilia <massicilia.nedir@outlook.com>
2019-12-04 01:17:17 +0100

    deployment db

--- src/main/java/esgi/com/exposition/config/WebSecurityConfig.java
+++ src/main/java/esgi/com/exposition/config/WebSecurityConfig.java
@@ -37,9 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**")
                 .permitAll()
                 .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin();
+                .authenticated();
     }
 
 
